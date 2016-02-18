8997b4678fb591adde46690712875ed82eebd04b
Bruno Furtado <brunofurtadoc@gmail.com>
2016-02-18 16:46:52 -0200

    Add House model to api app

--- api/models.py
+++ api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class House(models.Model):
+    create_at = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True, default='')
+    rent = models.DecimalField(max_digits=6, decimal_places=2)
+    condominium_association_fee = models.DecimalField(max_digits=6, decimal_places=2, default=0.00)
+    bedrooms = models.IntegerField()
+    bathrooms= models.IntegerField()
+    garage = models.IntegerField()
